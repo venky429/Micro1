80edaf3d7d6c73b17d7eb582a159eabec49e76c8
dvram <ramana0829@gmail.com>
2022-10-28 14:56:52 +0530

    281022_RestControllerChangedToControllerAndResponseBody

--- micro1/src/main/java/com/example/micro1/controller/M1Controller.java
+++ micro1/src/main/java/com/example/micro1/controller/M1Controller.java
@@ -1,28 +1,18 @@
 package com.example.micro1.controller;
 
-import java.util.List;
-
-//import org.apache.http.HttpStatus;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
 import com.example.micro1.Vo.M2;
 import com.example.micro1.Vo.Responce;
 import com.example.micro1.entities.Micro1;
 import com.example.micro1.service.M1Service;
-//import com.google.common.net.HttpHeaders;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
-@RestController
+import java.util.List;
+
+@Controller
+@ResponseBody
 @RequestMapping("/micro1")
 public class M1Controller {
 
